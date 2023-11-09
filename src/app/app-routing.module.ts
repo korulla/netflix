93874e9ce50e2b05d37a68dfaa875f5d26aea43c
korulla <korulla@users.noreply.github.com>
2023-11-09 19:41:04 +0530

    first Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'Search',component:SearchComponent},
+  {path:'Movie/:id',component:MovieDetailsComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
